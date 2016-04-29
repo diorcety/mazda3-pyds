dd6895fffe0dd0e03d78c6456854e975a51967f4
Yann Diorcet <diorcet.yann@gmail.com>
2016-04-29 20:44:44 +0200

    Make it works on Windows and Linux

--- run.bat
+++ run.bat
@@ -1,8 +1,9 @@
 @echo off
 SET root_path=%~dp0
+for /f %%i in ('python  -c "from distutils.sysconfig import get_python_lib; import sys; print get_python_lib().replace(sys.prefix, '/').replace('dist-', 'site-')"') do set PYTHON_SITE_PACKAGES=%%i
 setlocal
-SET PYTHONPATH=%root_path%\output\lib\site-packages
-SET PATH=%root_path%/output/bin/;%PATH%
+SET PYTHONPATH=%root_path%output\%PYTHON_SITE_PACKAGES%
+SET PATH=%root_path%output\bin;%PATH%
 echo %PYTHONPATH%
 python pyds/main.py
 endlocal
--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
   [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
 ROOT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-LD_LIBRARY_PATH="${ROOT_DIR}/output/lib" PYTHONPATH="${ROOT_DIR}/output/lib/site-packages" python ${ROOT_DIR}/pyds/main.py
+PYTHON_SITE_PACKAGES=`python  -c "from distutils.sysconfig import get_python_lib; import sys; print get_python_lib().replace(sys.prefix, '/').replace('dist-', 'site-')"`
+LD_LIBRARY_PATH="${ROOT_DIR}/output/lib" PYTHONPATH="${ROOT_DIR}/output/${PYTHON_SITE_PACKAGES}" python ${ROOT_DIR}/pyds/main.py
